dc965912edde73ff8f8bd51420bdaeb937f225c4
Yug <149261365+Tracy0718@users.noreply.github.com>
2025-03-27 15:24:04 +0530

    Add files via upload
    Designed basic GUI layout

---
+++ gui.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from scheduler import fcfs_scheduling
+from process_input import get_process_input
+
+class CPUSchedulerGUI:
+    def _init_(self, root):
+        self.root = root
+        self.root.title("CPU Scheduler Simulator")
+        self.process_list = []
+
+        self.label = tk.Label(root, text="CPU Scheduler", font=("Arial", 14))
+        self.label.pack()
+
+        self.add_process_button = tk.Button(root, text="Add Processes", command=self.add_processes)
+        self.add_process_button.pack()
+
+        self.run_fcfs_button = tk.Button(root, text="Run FCFS", command=self.run_fcfs)
+        self.run_fcfs_button.pack()
+
+        self.result_text = tk.Text(root, height=10, width=50)
+        self.result_text.pack()
+
+    def add_processes(self):
+        self.process_list = get_process_input()
+
+    def run_fcfs(self):
+        scheduled = fcfs_scheduling(self.process_list)
+        self.display_results(scheduled)
+
+    def display_results(self, processes):
+        self.result_text.delete('1.0', tk.END)
+        self.result_text.insert(tk.END, "PID\tArrival\tBurst\tWaiting\tTurnaround\n")
+        for p in processes:
+            self.result_text.insert(tk.END, f"{p.pid}\t{p.arrival_time}\t{p.burst_time}\t{p.waiting_time}\t{p.turnaround_time}\n")
+
+if _name_ == "_main_":
+    root = tk.Tk()
+    app = CPUSchedulerGUI(root)
+    root.mainloop()
